75410c9ec40bac4932af1243f0fe848c2c5ed2b0
David <carmichaelits@gmail.com>
2025-09-15 08:33:43 +0100

    remove solid dev tools line from config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import {defineConfig} from 'vite'
 import solidPlugin from 'vite-plugin-solid'
 import tailwindcss from "@tailwindcss/vite";
-// import devtools from 'solid-devtools/vite';
 
 export default defineConfig({
     plugins: [
